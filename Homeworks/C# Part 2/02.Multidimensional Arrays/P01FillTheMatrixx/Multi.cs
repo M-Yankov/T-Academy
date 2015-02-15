c94e69da2ebab58f272247e66af24331a86c6036
M-Yankov <pointblank12345@abv.bg>
2015-02-15 18:07:52 +0200

    some changes C# 2 multi arrays

--- Homeworks/C# Part 2/02.Multidimensional Arrays/P01FillTheMatrixx/Multi.cs
+++ Homeworks/C# Part 2/02.Multidimensional Arrays/P01FillTheMatrixx/Multi.cs
@@ -27,7 +27,7 @@ class Multi
         for (int i = 0; i < n; i++)
         {
             for (int z = 0; z < n; z++)
-            { 
+            {
                 multi[i, z] = ineer;
                 ineer++;
             }
@@ -38,20 +38,20 @@ class Multi
         int count = 1;
         for (int i = 0; i < n; i++)
         {
-            if(count % 2 != 0)
+            if (count % 2 != 0)
             {
                 for (int z = 0; z < n; z++)
                 {
-                    multi[i,z] = ineer;
+                    multi[i, z] = ineer;
                     ineer++;
                 }
                 count++;
             }
             else
             {
-                for (int p = n-1; p >= 0; p--)
+                for (int p = n - 1; p >= 0; p--)
                 {
-                    multi[i,p] = ineer;
+                    multi[i, p] = ineer;
                     ineer++;
                 }
                 count++;
@@ -59,7 +59,132 @@ class Multi
         }
         Console.WriteLine("\n\nb)");
         Print(multi);
+        Array.Clear(multi, 0, multi.Length);
 
+        ineer = 1;
+        int ime = 1;
+        int nameCol = 1;
+        int col = 0;
+        int row = multi.GetLength(1) - ime;
+        bool lastRow = true;
+        bool lastCol = false;
+
+        while (true)
+        {
+            if (lastRow)
+            {
+                row = multi.GetLength(1) - ime;
+                if (row < 0)
+                {
+                    row = 0;
+                    lastCol = true;
+                }
+
+            }
+            if (lastCol)
+            {
+                col = nameCol;
+            }
+            if (row == 0 && col == n - 1)
+            {
+                multi[n - 1, 0] = n * n;
+                break;
+            }
+            multi[col, row] = ineer;
+
+            ineer++;
+
+
+            if (row == n - 1)
+            {
+                ime++;
+                col = 0;
+                lastRow = true;
+
+            }
+            else if (col == n - 1)
+            {
+                nameCol++;
+                row = 0;
+                lastCol = true; // to repair code.
+            }
+            else
+            {
+                row++;
+                col++;
+                lastRow = false;
+                lastCol = false; // new added 
+            }
+        }
+        Console.WriteLine("\n\nc)");
+        Print(multi);
+        ineer = 1;
+        row = 0;
+        col = 0;
+        int dokade = 0;
+        bool exit = false;
+        while (true)
+        {
+            for (int z = 0; z < multi.GetLength(1) - dokade; z++)   // z < n - dokade
+            {
+                multi[col, z] = ineer;                    // |      /// array[ col , row ] ???
+                if (ineer == n * n)
+                {
+                    exit = true;
+                }
+                ineer++;                                  // |
+                row = z;                                  // V
+            }
+            if (exit)
+            {
+                break;
+            }
+            for (int c = 1; c < multi.GetLength(0) - dokade; c++)  /// ------->
+            {
+                multi[c, row] = ineer;
+                if (ineer == n * n)
+                {
+                    exit = true;
+                }
+                ineer++;
+                col = c;
+            }
+            if (exit)
+            {
+                break;
+            }
+            for (int u = row - 1; u > -1 + dokade; u--)    // ^
+            {                                               // |
+                multi[col, u] = ineer;                      // |
+                if (ineer == n * n)
+                {
+                    exit = true;
+                }
+                ineer++;                                    // |
+                row = u;                                    // |
+            }
+            if (exit)
+            {
+                break;
+            }
+            dokade++;
+            for (int le = col - 1; le > -1 + dokade; le--)     /// <-------   //// col - 1 - dokade
+            {
+                multi[le, row] = ineer;
+                if (ineer == n * n)
+                {
+                    exit = true;
+                }
+                ineer++;
+                col = le;
+            }
+            if (exit)
+            {
+                break;
+            }
+        }
+        Console.WriteLine("\n\nd)");
+        Print(multi);
     }
     static void Print(int[,] array)
     {
@@ -67,7 +192,7 @@ class Multi
         {
             for (int z = 0; z < array.GetLength(1); z++)
             {
-                Console.Write("{0,3}" ,array[z, i]);
+                Console.Write("{0,3}", array[z, i]);
             }
             Console.WriteLine();
         }
---
+++ Homeworks/C# Part 2/02.Multidimensional Arrays/P02MaxicalSum/Sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace P02MaxicalSum
+{
+    class Sum
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
