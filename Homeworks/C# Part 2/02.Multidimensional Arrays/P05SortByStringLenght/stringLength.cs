11b9e788ac0a3959728beea755166e71c31cdae5
M-Yankov <pointblank12345@abv.bg>
2015-02-18 00:02:44 +0200

    Problem05 added

---
+++ Homeworks/C# Part 2/02.Multidimensional Arrays/P05SortByStringLenght/stringLength.cs
@@ -0,0 +1,30 @@
+﻿/*Problem 5. Sort by string length
+
+    You are given an array of strings. Write a method that sorts the array by the length of its elements (the number of characters composing them).
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class StringLength
+{
+    static void Main()
+    {
+        List<string> textStrings = new List<string>();
+        textStrings.Add("Ivan");
+        textStrings.Add("Georgi_Traktora");
+        textStrings.Add("Pi");
+        textStrings.Add("Strahil"); // You can add many more strings here
+        var result = textStrings.OrderBy(x => x.Length); // Lambda expresion . I wrote this code for 3 mins :)
+        Console.WriteLine("Sorting...");
+        foreach (var i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
