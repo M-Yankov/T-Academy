6e8f9e029adf2f8edfc07937341f404dd21d0d6e
M-Yankov <pointblank12345@abv.bg>
2015-02-21 21:48:08 +0200

    Added Homework methods

--- Homeworks/C# Part 2/03.Methods/P08NumberAsArray/NumberAsArrrray.cs
+++ Homeworks/C# Part 2/03.Methods/P08NumberAsArray/NumberAsArrrray.cs
@@ -15,20 +15,63 @@ class NumberAsArrrray
     static void Main()
     {
         Console.Write("Enter fitst num: ");
-        string first = Console.ReadLine();
+        string first = Console.ReadLine();  // 152
         Console.Write("Enter second num: ");
-        string second = Console.ReadLine();
+        string second = Console.ReadLine(); // 111
         int num = first.Length > second.Length ? first.Length : second.Length;
         int[] array1 = new int[num];
         int[] array2 = new int[num];
+        Fill(array1, Convert.ToInt32(first));
+        Fill(array2, Convert.ToInt32(second));
+        Console.WriteLine("\nResult: ");
+        var final = Calculate(array1, array2, num);
+        Array.Reverse(final);
+        foreach (var i in final)
+        {
+
+            Console.Write(i);
+        }
+        Console.WriteLine();
+
     }
-    static void Fill(int[] arr , int num)
+    static void Fill(int[] arr, int num)
     {
-        for (int i = arr.Length -1; i >= 0; i--)
+        for (int i = 0; i < arr.Length; i++)
         {
             arr[i] = num % 10;
             num = num / 10;
         }
     }
-    static void Calculate(int[] a , int[] b ,sdaljwbefjks)
+    static int[] Calculate(int[] a, int[] b, int num)
+    {
+        int naYm = 0;
+        int[] result = new int[num];
+        int sum = 0;
+        for (int i = 0; i < num; i++)
+        {
+            sum = a[i] + b[i];
+            if (sum > 9)
+            {
+                result[i] = (sum % 10) + naYm; // 15 % 10 = 5;
+                naYm = 1; // 15 /10= 1 naYm is allays 1 ... 10,11,12,13,14,15,16,17,18 <-- that's all results from sum of two 0igits,
+            }
+            else
+            {
+                result[i] = sum + naYm;
+                naYm = 0;
+            }
+        }
+        if (naYm == 1)
+        {
+            int[] newArray = new int[result.Length + 1];
+            Array.Copy(result, newArray, result.Length);
+            newArray[newArray.Length - 1] = 1;
+            return newArray;
+        }
+        else
+        {
+
+            return result;
+        }
+    } // Yes 
 }
