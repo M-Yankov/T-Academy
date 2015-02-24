f119ee96094df4a5db555c5dd4805ab3a04ef506
M-Yankov <pointblank12345@abv.bg>
2015-02-24 19:06:43 +0200

    Objects

---
+++ Homeworks/C# Part 2/05.UsingClassesAndObjects/P06SumInteregs/Sums.cs
@@ -0,0 +1,21 @@
+﻿/*Problem 6. Sum integers
+
+    You are given a sequence of positive integer values written into a string, separated by spaces.
+    Write a function that reads these values from given string and calculates their sum.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Sums
+{
+    static void Main()
+    {
+        Console.Write("Enter numbers separeted by space: ");
+        int[] myArray = Console.ReadLine().Split(new string[] { " ", ",", ", " }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+        Console.WriteLine("{0}" , myArray.Sum());
+    }
+}
