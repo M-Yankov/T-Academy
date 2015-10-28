4d304cd32c2813b09301ab9e98c16a093ce2557f
M-Yankov <pointblank12345@abv.bg>
2015-10-28 21:51:31 +0200

    Additional materials on old exam

---
+++ Exams/ExamHQC/Niki/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Computers.UI
+{
+    using System;
+    using Computers.Models;
+
+    public class Program
+    {
+        private static ManufacturerFactory manufactuersFactory = new ManufacturerFactory();
+        private static ManufacturerType type;
+
+        public static void Main()
+        {
+            var userInput = Console.ReadLine();
+            Enum.TryParse(userInput, false, out type);
+            var manufacteur = manufactuersFactory.GetManufacturer(type);
+
+            while (true)
+            {
+                var c = Console.ReadLine();
+                if (c == null)
+                {
+                    break;
+                }
+
+                if (c.StartsWith("Exit"))
+                {
+                    break;
+                }
+
+                var cp = c.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (cp.Length != 2)
+                {
+                    Console.WriteLine("Invalid command!");
+                    continue;
+                }
+
+                var cn = cp[0];
+                var ca = int.Parse(cp[1]);
+
+                if (cn == "Charge")
+                {
+                    manufacteur.Laptop.ChargeBattery(ca);
+                }
+                else if (cn == "Process")
+                {
+                    manufacteur.Server.Process(ca);
+                }
+                else if (cn == "Play")
+                {
+                    manufacteur.PersonalComputer.Play(ca);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+            }
+        }
+    }
+}
