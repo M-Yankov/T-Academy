0a90eb32bb7e2d60b485d4b48e64109ea8bfcb1a
M-Yankov <pointblank12345@abv.bg>
2015-11-05 21:20:51 +0200

    Adding missing Tasks

---
+++ Homeworks/DataStructuresAlgorithms/04.HashTables/HashTablesSetDictionary/05.HashedSetStructure/HashedSet.cs
@@ -0,0 +1,104 @@
+﻿namespace HashedSetStructure
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using KeyValuePairStructure;
+
+    public class HashedSet<T> : IEnumerable<T>
+    {
+        private const int InitialSize = 16;
+        private HashTable<int, T> data;
+
+        public HashedSet()
+            : this(InitialSize)
+        {
+        }
+
+        public HashedSet(uint size)
+        {
+            this.data = new HashTable<int, T>(size);
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            try
+            {
+                this.data.Add(item.GetHashCode(), item);
+                this.Count++;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void Remove(T item)
+        {
+            int countBefore = this.data.Count;
+            this.data.Remove(item.GetHashCode());
+            int differance = countBefore - this.data.Count;
+            this.Count -= differance;
+        }
+
+        public T Find(T item)
+        {
+            return this.data.Find(item.GetHashCode());
+        }
+
+        public void Clear()
+        {
+            this.data.Clear();
+            this.Count = this.data.Count;
+        }
+
+        public void Union(HashedSet<T> unionWith)
+        {
+            int countBefore = this.data.Count;
+
+            foreach (var item in unionWith)
+            {
+                this.data.Add(item.GetHashCode(), item);
+            }
+
+            int differance = this.data.Count - countBefore;
+            this.Count += differance;
+        }
+
+        public void Intersect(HashedSet<T> unionWith)
+        {
+            var result = new HashTable<int, T>();
+            foreach (var item in unionWith)
+            {
+                try
+                {
+                    if (this.data.Find(item.GetHashCode()) != null)
+                    {
+                        result.Add(item.GetHashCode(), item);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            this.data = result;
+            this.Count = result.Count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.data)
+            {
+                yield return item.Value;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
