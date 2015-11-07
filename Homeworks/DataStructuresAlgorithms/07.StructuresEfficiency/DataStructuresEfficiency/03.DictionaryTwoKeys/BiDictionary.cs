a073cf4edece69838c96154a605f2724ea31a71a
M-Yankov <pointblank12345@abv.bg>
2015-11-07 19:22:25 +0200

    [DSA] data structures effect

---
+++ Homeworks/DataStructuresAlgorithms/07.StructuresEfficiency/DataStructuresEfficiency/03.DictionaryTwoKeys/BiDictionary.cs
@@ -0,0 +1,104 @@
+﻿namespace DictionaryTwoKeys
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BiDictionary<Key, Key2, TValue> where TValue : IComparable
+    {
+        private Dictionary<Key, TValue> dickData;
+        private Dictionary<Key2, TValue> secondData;
+
+        public BiDictionary()
+        {
+            this.dickData = new Dictionary<Key, TValue>();
+            this.secondData = new Dictionary<Key2, TValue>();
+        }
+
+        public void Add(Key key, TValue data)
+        {
+            if (this.dickData.ContainsKey(key))
+            {
+                throw new InvalidOperationException("Key Already exist");
+            }
+
+            this.dickData.Add(key, data);
+        }
+
+        public void Add(Key2 key, TValue data)
+        {
+            if (this.secondData.ContainsKey(key))
+            {
+                throw new InvalidOperationException("Key Already exist");
+            }
+
+            this.secondData.Add(key, data);
+        }
+
+        public void Add(Key firstKey, Key2 secondKey, TValue data)
+        {
+            if (this.secondData.ContainsKey(secondKey) || this.dickData.ContainsKey(firstKey))
+            {
+                throw new InvalidOperationException("Key Already exist");
+            }
+
+            this.dickData.Add(firstKey, data);
+            this.secondData.Add(secondKey, data);
+        }
+
+        public TValue this[Key firstKey]
+        {
+            get
+            {
+                try
+                {
+                    TValue value = this.dickData[firstKey];
+                    return value;
+                }
+                catch (Exception ex)
+                {
+                    throw new KeyNotFoundException("Key not found!", ex);
+                }
+            }
+        }
+
+        public TValue this[Key2 secondKey]
+        {
+            get
+            {
+                try
+                {
+                    TValue value = this.secondData[secondKey];
+                    return value;
+                }
+                catch (Exception ex)
+                {
+                    throw new KeyNotFoundException("Key not found!", ex);
+                }
+            }
+        }
+
+        public TValue this[Key firstKey, Key2 secondKey]
+        {
+            get
+            {
+                try
+                {
+                    TValue value = this[firstKey];
+                    return value;
+                }
+                catch (Exception ex)
+                {
+                    try
+                    {
+                        TValue value = this[secondKey];
+                        return value;
+                    }
+                    catch(Exception)
+                    {
+                        throw new KeyNotFoundException("Second key not found.", ex);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/DataStructuresAlgorithms/07.StructuresEfficiency/DataStructuresEfficiency/03.DictionaryTwoKeys/NoTimeForHomework.cs
@@ -0,0 +1,21 @@
+﻿namespace DictionaryTwoKeys
+{
+    using System;
+
+    public class NoTimeForHomework
+    {
+        public static void Main()
+        {
+            BiDictionary<int, string, decimal> mixMash = new BiDictionary<int, string, decimal>();
+            mixMash.Add(1, "DJ Ivan", 2.31M);
+            mixMash.Add(5, "DJ Doncho", 91321.123M);
+            mixMash.Add("DJ Dobry", 239401.233M);
+            mixMash.Add(2, 2.31M);
+            mixMash.Add("DJ LoopTheVoice", 3.14159M);
+
+            Console.WriteLine("{0:C2}", mixMash["DJ Dobry"]);
+            Console.WriteLine("{0:C2}", mixMash[5]);
+            Console.WriteLine("{0:C2}", mixMash[7]);
+        }
+    }
+}
