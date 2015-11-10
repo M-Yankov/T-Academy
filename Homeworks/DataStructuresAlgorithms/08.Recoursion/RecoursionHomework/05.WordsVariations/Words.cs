f57ec03cf6abd1d392d8df785201a45e6b5b699f
M-Yankov <pointblank12345@abv.bg>
2015-11-10 21:59:32 +0200

    Task 5 & 6

---
+++ Homeworks/DataStructuresAlgorithms/08.Recoursion/RecoursionHomework/05.WordsVariations/Words.cs
@@ -0,0 +1,47 @@
+﻿namespace WordsVariations
+{
+    using System.Text;
+
+    public class Words
+    {
+        private static int k = 2;
+        private static int n = 3;
+        private static int[] arr;
+        private static string[] words;
+
+        public static void Main()
+        {
+            arr = new int[n];
+            words = new string[] { "hi", "a", "b" };
+            GenerateVariationsNoReps(0);
+        }
+
+        private static void GenerateVariationsNoReps(int index)
+        {
+            if (index >= k)
+            {
+                PrintWords(arr);
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    arr[index] = i;
+                    GenerateVariationsNoReps(index + 1);
+                }
+            }
+        }
+
+        private static void PrintWords(int[] indexes)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < n - 1; i++)
+            {
+                result.Append(words[indexes[i]] + " ");
+            }
+
+            result.AppendLine();
+            System.Console.WriteLine("({0})", result.ToString().Trim());
+        }
+    }
+}
---
+++ Homeworks/DataStructuresAlgorithms/08.Recoursion/RecoursionHomework/06.WordCombinations/CombinationOfWords.cs
@@ -0,0 +1,43 @@
+﻿namespace WordCombinations
+{
+    public class CombinationOfWords
+    {
+        private static int k = 2;
+        private static int[] arr;
+        private static string[] words;
+
+        public static void Main()
+        {
+            words = new string[] { "test", "rock", "fun", "pesho", "gosho" };
+            arr = new int[words.Length];
+            Comb(0, 0);
+        }
+
+        private static void Comb(int index, int start)
+        {
+            if (index >= k)
+            {
+                PrintWords(arr);
+            }
+            else
+            {
+                for (int i = start; i < arr.Length; i++)
+                {
+                    arr[index] = i;
+                    Comb(index + 1, i + 1);
+                }
+            }
+        }
+
+        private static void PrintWords(int[] arr)
+        {
+            System.Text.StringBuilder result = new System.Text.StringBuilder();
+            for (int i = 0; i < k; i++)
+            {
+                result.Append(words[arr[i]] + " ");
+            }
+
+            System.Console.WriteLine(result.ToString());
+        }
+    }
+}
