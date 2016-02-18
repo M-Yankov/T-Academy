5744c2ad6bdafc8deeec5abae0f76104e9ff1b9d
M-Yankov <pointblank12345@abv.bg>
2016-02-19 00:33:54 +0200

    SignalR homework - chat system

---
+++ Homeworks/ASP_NET_MVC/06.SignalR/ChatingSystem/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ChatingSystem.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            this.Clients.All.addMessage(message, this.Context.User.Identity.Name);
+        }
+    }
+}
---
+++ Homeworks/ASP_NET_MVC/06.SignalR/ChatingSystem/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatingSystem.Startup))]
+namespace ChatingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
